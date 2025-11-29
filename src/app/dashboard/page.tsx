029be4e3e2a66318869cac5814baedf715b90545
Jose Gomes <jgomestkd@gmail.com>
2025-11-29 16:12:46 -0300

    feat: add container and footer

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from "@/components/ui/container";
+
+export default function DashboardPage() {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+}
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,21 @@
 export default function Footer() {
-    return (
-        <footer>
-            Footer
-        </footer>
-    );
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer
+      className={
+        "w-full flex items-center px-4 py-4 bg-white h-20 shadow-[0_-5px_10px_rgba(0,0,0,0.1)] relative z-50"
+      }
+    >
+      <div
+        className={"w-full flex items-center justify-center max-w-7xl mx-auto"}
+      >
+        <p className={"text-center"}>
+          <span className={"text-blue-500 font-bold"}>DEV</span>
+          <span className={"font-bold"}>Controle</span> &copy; {currentYear}{" "}
+          &bull; Todos os direitos reservados.{" "}
+        </p>
+      </div>
+    </footer>
+  );
 }
